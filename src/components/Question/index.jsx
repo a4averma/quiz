90e7c3731f343d8750ea0ec3a57ce7c64f5e4ce2
Aditya Verma <siradityaverma@gmail.com>
2021-07-24 19:25:08 +0530

    Add transitions and animations

--- src/components/Question/index.jsx
+++ src/components/Question/index.jsx
@@ -1,11 +1,18 @@
 import Answer from "./Answer";
 import { useQuestions } from "../../context/Question/action";
 import "./style.scss";
+import { useLayoutEffect } from "react";
 
 export default function Question() {
   const { questions, currentQuestion } = useQuestions();
+  useLayoutEffect(() => {
+    document.getElementById("question").classList.remove("fade");
+    setTimeout(() => {
+      document.getElementById("question").classList.add("fade");
+    }, 200);
+  }, [currentQuestion]);
   return (
-    <section className="padding">
+    <section className="padding opacity" id="question">
       <h6 className="question">{questions[currentQuestion].title}</h6>
       <div className="answers">
         {questions[currentQuestion].options.map((option, index) => (
--- src/components/Stats/index.jsx
+++ src/components/Stats/index.jsx
@@ -1,6 +1,7 @@
 import { useQuestions } from "../../context/Question/action";
 import "./styles.scss";
 import { FaCrosshairs } from "react-icons/all";
+import { useEffect } from "react";
 
 export default function Stats() {
   const {
@@ -9,6 +10,14 @@ export default function Stats() {
     answeredCorrectly,
     currentQuestionAnswer,
   } = useQuestions();
+  useEffect(() => {
+    if (answeredCorrectly > 0) {
+      document.getElementById("spin").classList.remove("spin");
+      setTimeout(() => {
+        document.getElementById("spin").classList.add("spin");
+      }, 200);
+    }
+  }, [answeredCorrectly]);
   return (
     <header className="stats">
       {questions.length === currentQuestion + 1 && currentQuestionAnswer ? (
@@ -20,7 +29,11 @@ export default function Stats() {
       )}
 
       <p className="percent">
-        <FaCrosshairs /> {(answeredCorrectly / questions.length) * 100}%
+        <FaCrosshairs />{" "}
+        <span className="spin" id="spin">
+          {(answeredCorrectly / questions.length) * 100}
+        </span>
+        %
       </p>
     </header>
   );
